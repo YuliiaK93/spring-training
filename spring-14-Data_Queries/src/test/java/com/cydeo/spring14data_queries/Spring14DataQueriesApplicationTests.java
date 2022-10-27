d09162898c4438e37b1d9e425521b20885cbb6d8
Yuliia <yulia.karnoza@gmail.com>
2022-10-27 12:43:52 -0400

    spring-14

---
+++ spring-14-Data_Queries/src/test/java/com/cydeo/spring14data_queries/Spring14DataQueriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring14data_queries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring14DataQueriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
